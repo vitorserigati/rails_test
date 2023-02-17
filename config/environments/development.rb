9ba8b3651f41e8dbb464a9cbbb25332e8fde0d62
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-02-17 15:33:52 -0300

    changed some configs for devise_token_auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,12 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Letter Opener
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  # Devise
+  config.action_mailer.default_urloptions = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
