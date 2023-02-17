d763a7d4d08695de1a2bcf248c819e5ca4b384c3
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-02-17 15:41:58 -0300

    generated the devise_token_auth install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/api_constraints'
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api, defaults: { format: :json } do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
