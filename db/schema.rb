56dfeb861ad81eb62522c09fcb8443ec8ea36d4a
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2023-02-19 01:51:33 -0300

    adding uuid as id for users

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_184128) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
